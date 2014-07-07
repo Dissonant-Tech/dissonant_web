65375c6f9ce3623b30f9c388eceaf48675a0675f
David Rodriguez <dissonant.tech@gmail.com>
2014-07-06 23:11:54 -0400

    Basic setup. index.html and static files loading

--- dissonant_tech/views.py
+++ dissonant_tech/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import Context, loader
 
 def index(request):
-    return HttpResponse("Hello, world!")
+    t = loader.get_template('index.html')
+    c = Context({"page": "Home"})
+    return HttpResponse(t.render(c));
