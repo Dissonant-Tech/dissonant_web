c4376c9d61eeb84f87e7846fcbff3cd885544844
David Rodriguez <dissonant.tech@gmail.com>
2014-07-19 00:00:05 -0400

    Moving over to bower.
    Added new urls in django urls.py. Moving over to using static and
    components from base directory as opposed to everything from dir.

--- dissonant_tech/views.py
+++ dissonant_tech/views.py
@@ -6,3 +6,8 @@ def index(request):
     t = loader.get_template('index.html')
     c = Context({"page": "Home"})
     return HttpResponse(t.render(c));
+
+def projects(request):
+    t = loader.get_template('projects.html')
+    c = Context({"page": "Projects"})
+    return HttpResponse(t.render(c));
