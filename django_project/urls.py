9d7780e96837366a4d85b22b182ed71f23cafd87
David Rodriguez <dissonant.tech@gmail.com>
2014-07-21 10:01:52 -0400

    Contact page mostly finished

--- django_project/urls.py
+++ django_project/urls.py
@@ -16,6 +16,5 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
 )
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.COMPONENT_URL, document_root=settings.COMPONENT_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.COMPONENT_URL, document_root=settings.COMPONENT_ROOT)
