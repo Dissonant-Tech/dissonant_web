ddb64be202f83815eec64dab62c97e1abd8383de
David Rodriguez <dissonant.tech@gmail.com>
2014-07-26 19:08:04 -0400

    Split upp INSTALLED_APPS in settings.py

--- dissonant_website/settings.py
+++ dissonant_website/settings.py
@@ -29,16 +29,25 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = (
+DEFAULT_APPS = (
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'dissonant_tech',
-    'south',
 )
+
+THIRD_PARTY_APPS = (
+        'south',
+)
+
+LOCAL_APPS = (
+        'dissonant_tech',
+)
+
+
+INSTALLED_APPS = DEFAULT_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
