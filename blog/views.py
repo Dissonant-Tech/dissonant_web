f2ba3b5416cbbb8e65beaaa1cf8a6730d184a5dd
David Rodriguez <dissonant.tech@gmail.com>
2014-07-28 13:09:15 -0400

    Changed Admin BlogPost to Article

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,11 @@
-from django.shortcuts import render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.template import Context, loader
+
+from blog.models import Category
 
 def index(request):
     t = loader.get_template('index.html')
     c = Context({"page": "Home"})
-    return HttpResponse(t.render(c));
-
-def projects(request):
-    t = loader.get_template('projects.html')
-    c = Context({"page": "Projects"})
     return HttpResponse(t.render(c));
