fa2a53fece435d3a96690406fd0729ea3c52f680
David Rodriguez <dissonant.tech@gmail.com>
2014-07-28 12:31:11 -0400

    Changed dissonant_tech folder to blog
    The move from dissonant_tech folder to blog better represents what
    exactly the dissonant_tech app was previously doing, as well as helping
    to better organize the website in the future

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from django import forms
-from dissonant_tech.models import Category, BlogPost
+from blog.models import Category, BlogPost
 from pagedown.widgets import AdminPagedownWidget
-from dissonant_tech.models import Category, BlogPost
+from blog.models import Category, BlogPost
 
 # Register your models here.
 
--- blog/migrations/0001_initial.py
+++ blog/migrations/0001_initial.py
@@ -17,4 +17,4 @@ class Migration(SchemaMigration):
         
     }
 
-    complete_apps = ['dissonant_tech']
+    complete_apps = ['blog']
