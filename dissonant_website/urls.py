f2f501b52db86f7513dbf9d354e036360762f633
David Rodriguez <dissonant.tech@gmail.com>
2014-07-28 15:57:39 -0400

    Fixed admin urls not working.

--- dissonant_website/urls.py
+++ dissonant_website/urls.py
@@ -8,11 +8,9 @@ from main import views
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'django_project.views.home', name='home'),
     url(r'^$', views.index, name='index'),
-    url(r'^blog/', include('blog.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^blog/', include('blog.urls')),
 )
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
