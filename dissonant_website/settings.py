6a8fd7befd0d4cd3d5cca48d6202e239550d389f
David Rodriguez <dissonant.tech@gmail.com>
2015-02-11 15:23:29 -0500

    Deleted history for settings.py and db.sqlite
    In preperation to open sourcing my webpage I've moved sensitive data
    outside of the repository and deleted the history of said data as well.

---
+++ dissonant_website/settings.py
@@ -0,0 +1,107 @@
+"""
+Django settings for dissonant_website project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.6/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.6/ref/settings/
+"""
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+from dissonant_website.secrets import *
+
+import os, socket
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+
+# SECURITY WARNING: don't run with debug turned on in production!
+if socket.gethostname() == 'dissonant-tech':
+    DEBUG = False
+    TEMPLATE_DEBUG = False
+else:
+    DEBUG = True
+    TEMPLATE_DEBUG = True
+
+
+ALLOWED_HOSTS = [
+        '.dissonant-tech.com'
+        ]
+
+
+# Application definition
+
+DEFAULT_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+)
+
+THIRD_PARTY_APPS = (
+        'pagedown',
+        'markdown',
+        'pygments'
+)
+
+LOCAL_APPS = (
+        'dissonant_website',
+        'blog',
+)
+
+
+INSTALLED_APPS = DEFAULT_APPS + THIRD_PARTY_APPS + LOCAL_APPS
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+TEMPLATE_LOADERS = (
+        'django.template.loaders.filesystem.Loader',
+        'django.template.loaders.app_directories.Loader',
+)
+
+ROOT_URLCONF = 'dissonant_website.urls'
+
+WSGI_APPLICATION = 'dissonant_website.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.6/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.6/howto/static-files/
+
+if DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, "static")
+else:
+    STATIC_ROOT = "/srv/www/dissonant-tech.com/static/"
+
+STATIC_URL = '/static/'
