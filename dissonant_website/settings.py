396bee4b4800c8b576d822ed2554ff15fcb61446
David Rodriguez <dissonant.tech@gmail.com>
2015-10-19 23:45:42 -0400

    Add django Debug toolbar

--- dissonant_website/settings.py
+++ dissonant_website/settings.py
@@ -47,6 +47,7 @@ THIRD_PARTY_APPS = (
         'markdown',
         'pygments',
         'compressor',
+        'debug_toolbar',
         )
 
 LOCAL_APPS = (
@@ -57,6 +58,7 @@ LOCAL_APPS = (
 INSTALLED_APPS = DEFAULT_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
 MIDDLEWARE_CLASSES = (
+        'debug_toolbar.middleware.DebugToolbarMiddleware',
         'django.contrib.sessions.middleware.SessionMiddleware',
         'django.middleware.common.CommonMiddleware',
         'django.middleware.csrf.CsrfViewMiddleware',
@@ -101,6 +103,22 @@ if DEBUG:
                 'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
                 }
             }
+
+DEBUG_TOOLBAR_PANELS = [
+        'debug_toolbar.panels.versions.VersionsPanel',
+        'debug_toolbar.panels.timer.TimerPanel',
+        'debug_toolbar.panels.settings.SettingsPanel',
+        'debug_toolbar.panels.headers.HeadersPanel',
+        'debug_toolbar.panels.request.RequestPanel',
+        'debug_toolbar.panels.sql.SQLPanel',
+        'debug_toolbar.panels.staticfiles.StaticFilesPanel',
+        'debug_toolbar.panels.templates.TemplatesPanel',
+        'debug_toolbar.panels.cache.CachePanel',
+        'debug_toolbar.panels.signals.SignalsPanel',
+        'debug_toolbar.panels.logging.LoggingPanel',
+        'debug_toolbar.panels.redirects.RedirectsPanel',
+        'debug_toolbar.panels.profiling.ProfilingPanel',
+        ]
 
 
 # Internationalization
