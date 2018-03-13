37e32b882b56f97b68eff70337c09f37dd9ef26d
David Rodriguez <dissonant.tech@gmail.com>
2018-03-13 13:44:38 -0400

    Update deployment and gitignore

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,6 +6,10 @@ sleep 5
 python manage.py migrate                  # Apply database migrations
 python manage.py collectstatic --noinput  # Collect static files
 
+echo "from django.contrib.auth.models import User;" \
+     "User.objects.create_superuser('$ADMIN_USER', '$ADMIN_EMAIL', '$ADMIN_PASS')" \
+     | python manage.py shell
+
 # Start Gunicorn processes
 echo Starting Gunicorn.
 exec gunicorn dissonant_website.wsgi:application \
